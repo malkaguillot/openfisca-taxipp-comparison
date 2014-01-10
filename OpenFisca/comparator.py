8e7015efeb90ae0cbca20a77c596d1cb163c38a4
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2014-01-10 08:18:24 +0100

    Testing SurveySimulation in comparator

--- OpenFisca/comparator.py
+++ OpenFisca/comparator.py
@@ -44,6 +44,8 @@ def compare(year = 2006):
     simulation = ScenarioSimulation()
     simulation.set_config(year = year, reforme = False, nmen = 3, maxrev = 100000, x_axis = 'sali')
     # Add husband/wife on the same tax sheet (foyer).
+
+    fill_with_ipp_input(simulation, df)
     
     simulation.scenario.addIndiv(1, datetime.date(1975, 1, 1), 'conj', 'part')
     simulation.set_param()
@@ -54,5 +56,50 @@ def compare(year = 2006):
     print df.to_string()
 
 
+def fill_with_ipp_input(df):
+
+    print df.columns
+    openfisca_survey = df.loc[:,["id_foyf","id_indiv", "age", "marie", "mat", "decl", "conj", "pac", "cadre", "public" ]]
+    print openfisca_survey.to_string()
+    from pandas import DataFrame
+    d = DataFrame()
+    
+    # simple rename
+    
+    cols = {"id_foyf" : "idfoy", "id_indiv" : "noi"}
+    openfisca_survey.rename( columns=cols, inplace=True)
+    
+    # TODO: REMOVE IF NOT CELIB
+    openfisca_survey["idmen"] = openfisca_survey["idfoy"] 
+    openfisca_survey["idfam"] = openfisca_survey["idfoy"] 
+
+    
+    def _quifoy(col):
+        quimen = col["conj"]==1 + col["pac"]==1
+        return quimen
+    
+    openfisca_survey["quifoy"] = openfisca_survey.apply(_quifoy, axis=1).astype(int)
+    openfisca_survey.drop(["decl", "conj", "pac"], axis=1)
+    print openfisca_survey.columns
+    print openfisca_survey.to_string()
+    
+    # TODO: REMOVE (just for testing import
+    openfisca_survey["quimen"] = openfisca_survey["quifoy"]
+    openfisca_survey["quifam"] = openfisca_survey["quifoy"]
+    ## UNTL HERE
+    
+    
+    simulation = SurveySimulation()
+    simulation.set_config(year=2006, survey_filename=openfisca_survey)
+    simulation.set_param()
+    simulation.compute()
+    print simulation.input_table.table.describe().to_string()    
+    print simulation.output_table.table.describe().to_string()    
+    
+    return openfisca_survey
+
+
 if __name__ == '__main__':
-    compare(2011)
+#    compare(2011)
+    df = read_stata("../Taxipp/base_IPP_celib.dta")
+    fill_with_ipp_input(df)
