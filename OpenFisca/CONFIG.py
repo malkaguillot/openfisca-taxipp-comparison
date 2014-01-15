3e3bce52e3009e1bd27e4df1f2fc020daf8bb01e
Guillot <malkaguillot@gmail.com>
2014-01-15 10:37:57 +0100

    Essai 2

---
+++ OpenFisca/CONFIG.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# Path of your repository
+path_comparison = # User's path for openfisca-taxipp-comparison respo  
+path_stata = # User's path for Stata 
+
+paths = {
+         'do_in' : path_comparison + 'OpenFisca/test.do',
+         'do_out': path_comparison + 'OpenFisca/test_out.do',
+         'stata' : path_stata,
+         'dta_out' : path_comparison + 'Taxipp/base_IPP',
+         'repo_to_stata' :  "C:\TaxIPP-Life\openfisca-taxipp-comparison\Taxipp"
+          }
